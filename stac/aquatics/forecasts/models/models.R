f58f8d48803065689b2ea11a6ce1f798bf671034
addelany <addelany@gmail.com>
2023-05-23 15:01:50 -0500

    restructure files and make automated model JSON

--- stac/aquatics/forecasts/models/models.R
+++ stac/aquatics/forecasts/models/models.R
@@ -1,12 +1,12 @@
-build_model <- function(model_id, description) {
+build_model <- function(model_id, team_name, model_description, first_name, last_name, email) {
 
 
   meta <- list(
-  "description" = "Python-based implementation of Meta's Prophet Model, forecasting aquatics variables using univariate timeseries only.",
+  "description" = model_description,
   "stac_version"= "1.0.0",
   "stac_extensions"= list(),
   "type"= "Feature",
-  "id"= "cb_prophet",
+  "id"= model_id,
   "bbox"=
         list(-156.6194, 17.9696, -66.7987,  71.2824),
   "geometry"= list(
@@ -24,25 +24,25 @@ build_model <- function(model_id, description) {
   "properties"= list(
     "datetime"= "2023-04-26T00=00=00Z"
   ),
-  "collection"= "aquatics",
+  "collection"= "forecast",
   "links"= list(
     list(
       "rel"= "collection",
-      "href"= "../aquatics.json",
+      "href"= "../forecst.json",
       "type"= "application/json",
       "title"= "Aquatics Forecasts"
     ),
     list(
       "rel"= "root",
-      "href"= "../../catalog.json",
+      "href"= "../../../catalog.json",
       "type"= "application/json",
       "title"= "EFI Forecast Catalog"
     ),
     list(
       "rel"= "parent",
-      "href"= "../aquatics.json",
+      "href"= "../forecast.json",
       "type"= "application/json",
-      "title"= "Aquatics Prophet Model"
+      "title"= "Aquatics Forecasts"
     )
   ),
   "assets"= list(
@@ -52,8 +52,8 @@ build_model <- function(model_id, description) {
                      "aquatics/model_id=", model_id,
                      "?endpoint_override=sdsc.osn.xsede.org"),
       "type"= "application/x-parquet",
-      "title"= "Trivial Forecasts using Meta's Prophet Model",
-      "description"= readr::read_file("stac/aquatics/models/asset-description.Rmd")
+      "title"= team_name,
+      "description"= readr::read_file("stac/aquatics/forecasts/models/asset-description.Rmd")
     )
   ),
    "license"= "CC0-1.0",
@@ -65,8 +65,8 @@ build_model <- function(model_id, description) {
   ),
   "providers"= list(
     list(
-      "url"= "https=//carlboettiger.info",
-      "name"= "Carl Boettiger",
+      "url"= email,
+      "name"= paste(first_name,last_name),
       "roles"= list(
         "producer",
         "processor",
@@ -74,7 +74,7 @@ build_model <- function(model_id, description) {
       )
     ),
     list(
-      "url"= "https=//ecoforecast.org",
+      "url"= "https://ecoforecast.org",
       "name"= "Ecoforecast Challenge",
       "roles"= list(
         "host"
@@ -84,7 +84,7 @@ build_model <- function(model_id, description) {
 )
 
 
-  dest <- "stac/aquatics/models/"
+  dest <- "stac/aquatics/forecasts/models/"
   json <- file.path(dest, paste0(model_id, ".json"))
 
   jsonlite::write_json(meta,
@@ -96,6 +96,30 @@ build_model <- function(model_id, description) {
 
 }
 
-# loop over model ids and extract components
-build_model("cb_prophet", "")
+## read in model documentation and only grab models for Aquatics theme
+library(tidyverse)
+library(stac4cast)
+library(reticulate)
 
+model_docs <- read_csv('NEON_Challenge_Registration_2023-05-23.csv')
+
+aquatic_models <- model_docs |>
+  filter(Theme == 'Aquatic Ecosystems') |>
+  select(`team-name`, `First Name`, `Last Name`, `Email address`, `model-id`, `model-description`) |>
+  rename(team.name = `team-name`, first.name = `First Name`, last.name = `Last Name`, email = `Email address`,
+         model.id = `model-id`, model.description = `model-description`) |>
+  mutate(model.description = ifelse(is.na(model.description),'',model.description))
+
+# loop over model ids and extract components
+for (i in seq.int(1,nrow(aquatic_models))){
+  #print(aquatic_models[i,'model.description'])
+
+  build_model(model_id = aquatic_models$model.id[i],
+              team_name = aquatic_models$team.name[i],
+              model_description = aquatic_models[i,'model.description'],
+              first_name = aquatic_models$first.name[i],
+              last_name = aquatic_models$last.name[i],
+              email = aquatic_models$email[i])
+}
+
+#build_model("cb_prophet", "")
