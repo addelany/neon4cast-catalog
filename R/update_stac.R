4857881ffe8cc1f447fa65d60f8588521a992609
addelany <addelany@gmail.com>
2023-07-05 14:12:16 -0500

    create initial stac automation workflow

---
+++ R/update_stac.R
@@ -0,0 +1,30 @@
+## AQUATICS
+# forecast
+source('stac/aquatics/forecasts/models/models.R')
+source('stac/aquatics/forecasts/forecast.R')
+
+# scores
+source('stac/aquatics/scores/models/models.R')
+source('stac/aquatics/scores/forecast.R')
+
+# aquatics landing page
+source('stac/aquatics/aquatics.R')
+
+
+## TERRESTRIAL
+# Pending
+
+## BEETLES
+# Pending
+
+## PHENOLOGY
+# Pending
+
+## NOAA
+# Pending
+
+## TICKS
+# Pending
+
+### BUILD COLLECTION JSON
+source('stac/catalog.R')
